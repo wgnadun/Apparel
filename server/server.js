b0e906f7e841654e1541491f39c00baad2fcac26
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-06-20 00:34:45 +0530

    start apperal project :  set data base configurations and install node modules

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+mongoose
+     .connect(
+        "mongodb+srv://wgnadundananjaya:SpkTnNzPrjb8mBYT@apperal.np1uklx.mongodb.net/"
+     ).then(() =>console.log('MongoDB connected')).catch(error => console.log(error));
+
+
+     const app = express('');
+     const PORT = process.env.PORT || 5000;
+
+     app.use(
+        cors({
+            origin: 'http://localhost:5173/',
+            methods : ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders : [
+                'Content-Type',
+                'Authorization',
+                'Cache-Control',
+                'Expires',
+                'Pragma'
+            ],
+            credentials: true
+        })
+     );
+
+     app.use(express.json());
+     app.use(cookieParser());
+
+     app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+     });        
