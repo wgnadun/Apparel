907d23d7936bdf9b8d41ca1f72d77d73da1e0945
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-06-25 17:29:02 +0530

    create auth route,middleware,controller and and update server.js

---
+++ server/middleware/auth/auth-middleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+//auth middleware
+const authMiddleware = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token)
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorised user!",
+    });
+
+  try {
+    const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Unauthorised user!",
+    });
+  }
+};
+
+module.exports = authMiddleware;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-
+const authRouter =require('./routes/auth/auth-routes')
 
 mongoose
      .connect(
@@ -30,6 +30,8 @@ mongoose
 
      app.use(express.json());
      app.use(cookieParser());
+
+     app.use('/api/auth',authRouter);
 
      app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
