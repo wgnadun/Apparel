6a322ec10ec2f02f453e474a4855832f4b5d8d7d
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-08-04 16:40:14 +0530

    continue address-controller

--- server/controllers/shop/address-controller.js
+++ server/controllers/shop/address-controller.js
@@ -1,8 +1,32 @@
-
+const Address = require('../../models/address');
 
 const addAddress = async(req,res)=>{
     try {
+
+        const {userId, address,city,pincode,phone,notes} = req.body;
+
+        if(!userId || !address || !city || !pincode || !phone || !notes){
+            return res.status(400).json({
+                success : false,
+                message : 'Invalid data provided !'
+            })
+        }
+
+        const newlycreatedAddress = new Address({
+            userId,
+            address,
+            city,
+            pincode,
+            phone,
+            notes
+        })
+
+        await newlycreatedAddress.save();
         
+        res.status(201).json({
+            success : true,
+            data : newlycreatedAddress
+        })
     } catch (e) {
         console.log(e);
         res.status(500).json({
@@ -14,7 +38,21 @@ const addAddress = async(req,res)=>{
 
 const fetchAllAddress = async(req,res)=>{
     try {
-        
+        const {userId} =req.params;
+        if(!userId){
+            return res.status(400).json({
+                success : false,
+                message : 'user Id is required!'
+            })
+        }
+
+        const addressesList = await Address.find({userId});
+
+        res.status(200).json({
+            success : true,
+            data : addressesList
+        })
+
     } catch (e) {
         console.log(e);
         res.status(500).json({
@@ -26,7 +64,35 @@ const fetchAllAddress = async(req,res)=>{
 
 const editAddress = async(req,res)=>{
     try {
-        
+        const {userId,addressId} = req.params;
+        const formData = req.body;
+
+         if(!userId || !addressId){
+            return res.status(400).json({
+                success : false,
+                message : 'user Id and address Id are required!'
+            })
+        }
+
+        const address = await Address.findOneAndUpdate({
+            _id: addressId, userId
+        },formData, 
+        {
+            new: true
+        }
+    );
+
+    if(!address){
+        return res.status(404).json({
+            success : false,
+            message : 'Address not found!'
+        })
+    }
+        res.status(200).json({
+            success : true,
+            data : address
+        })
+
     } catch (e) {
         console.log(e);
         res.status(500).json({
@@ -38,7 +104,29 @@ const editAddress = async(req,res)=>{
 
 const deleteAddress = async(req,res)=>{
     try {
-        
+       const {userId,addressId} = req.params;
+        if(!userId || !addressId){
+            return res.status(400).json({
+                success : false,
+                message : 'user Id and address Id are required!'
+            })
+        }
+    
+        const address = await Address.findOneAndDelete({
+            _id : addressId, userId
+        })
+
+      if(!address){
+        return res.status(404).json({
+            success : false,
+            message : 'Address not found!'
+        })
+    }
+        res.status(200).json({
+            success : true,
+            message : 'Address deleted successfully!'
+        })
+
     } catch (e) {
         console.log(e);
         res.status(500).json({
