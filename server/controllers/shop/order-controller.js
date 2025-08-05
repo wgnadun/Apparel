add843b04d047ae4eea22d3d9770c9b83325bba8
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-08-05 19:02:25 +0530

    create order controller and routes

---
+++ server/controllers/shop/order-controller.js
@@ -0,0 +1,103 @@
+const paypal = require('../../helpers/paypal');
+const Order = require('../../models/Order');
+const order = require('../../models/Order')
+
+const createOrder = async (req, res)=>{
+    try {
+        const {
+            userId, 
+            cartItems,
+            addressInfo,
+            OrderStatus,
+            paymentMethod,
+            paymentStatus,
+            totalAmount,
+            orderDate,
+            orderUpdateDate,
+            paymentId,
+            payerId
+        } = req.body;
+
+    const create_payment_json ={
+        intent: 'sale',
+        payer : {
+            payment_method:'paypal'
+        },
+        redirect_urls:{
+            return_url: "http://localhost:5173/shop/paypal-return",
+            cancel_url: "http://localhost:5173/shop/paypal-cancel",
+        },
+        transactions: [
+        {
+          item_list: {
+            items: cartItems.map((item) => ({
+              name: item.title,
+              sku: item.productId,
+              price: item.price.toFixed(2),
+              currency: "USD",
+              quantity: item.quantity,
+            })),
+          },
+          amount: {
+            currency: "USD",
+            total: totalAmount.toFixed(2),
+          },
+          description: "description",
+        },
+      ],
+    }
+
+    paypal.payments.create(create_payment_json,async(error,paymentInfo)=>{
+        if(error){
+            console.log(error)
+            return res.status(500).json({
+                success:false,
+                message :'Error while creating paypal payment'
+            })
+        }else{
+            const newlyCreatedOrder = new Order({
+                userId, 
+                cartItems,
+                addressInfo,
+                OrderStatus,
+                paymentMethod,
+                paymentStatus,
+                totalAmount,
+                orderDate,
+                orderUpdateDate,
+                paymentId,
+                payerId
+            })
+            await newlyCreatedOrder.save();
+            const approvalURL = paymentInfo.links.find(link=>link.rel ==='approval_url').href;
+
+            res.status(201).json({
+                success: true,
+                approvalURL,
+                orderId: newlyCreatedOrder._id
+            })
+        }
+    })
+
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success:false,
+            message:'Some error occured!'
+        })
+    }
+}
+const capturePayment = async (req, res)=>{
+    try {
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success:false,
+            message:'Some error occured!'
+        })
+    }
+}
+
+module.exports ={createOrder,capturePayment}
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const adminProductsRouter = require('./routes/admin/products-routes');
 const shopProductsRouter = require('./routes/shop/products-routes');
 const shopCartRouter = require('./routes/shop/cart-routes');
 const shopAddressRouter = require('./routes/shop/address-routes');
+const shopOrderRouter = require('./routes/shop/order-routes');
 mongoose
      .connect(
         "mongodb+srv://wgnadundananjaya:SpkTnNzPrjb8mBYT@apperal.np1uklx.mongodb.net/"
@@ -39,6 +40,7 @@ mongoose
      app.use('/api/shop/products',shopProductsRouter);
      app.use('/api/shop/cart',shopCartRouter);
      app.use('/api/shop/address',shopAddressRouter);
+     app.use('/api/shop/order',shopOrderRouter);
 
      app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
