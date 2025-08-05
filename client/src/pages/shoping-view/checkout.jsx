4e42dea469e96c35e4fdd3438992df197e86bd7c
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-08-05 10:01:32 +0530

    render th cart items in checkout page

--- client/src/pages/shoping-view/checkout.jsx
+++ client/src/pages/shoping-view/checkout.jsx
@@ -1,6 +1,28 @@
+import { useSelector } from 'react-redux';
+import img from '../../assets/account.jpg';
+import Address from './address';
+import UserCartItemsContent from '@/components/shopping-view/cart-items-content';
+
 function ShoppingCheckout() {
+    const {cartItems} = useSelector((state)=>state.shopCart);
+    console.log(cartItems);
     return(
-        <div>shopping Checkout</div>
+        <div className="flex flex-col">
+            <div className="relative h-[300px] w-full overflow-hidden">
+                <img src={img} alt="Account" className="object-cover object-center w-full h-full" />
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 mt-5 p-5">
+                <Address />
+                <div className='flex flex-col gap-4'>
+                    {cartItems && cartItems.length > 0
+                        ? cartItems.map((item, idx) => (
+                            <UserCartItemsContent key={item.productId || idx} cartItems={item} />
+                        ))
+                        : null}
+                </div>
+            </div>
+            
+        </div>
     );
     
 }
