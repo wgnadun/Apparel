5d628b85b47f9735e9058da7ec04aabfa80e4a84
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-08-05 23:08:27 +0530

    paypal config

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,9 +1,9 @@
-const paypal = require('@paypal/checkout-server-sdk');
+const paypal = require('paypal-rest-sdk');
 
 paypal.configure({
-    mode: 'sandbox', // or 'live' for production
-    client_id: 'AfStiWLxaD4yEsZXiqLJcyUPTZZr8ykW0BoLPsBtWERO0OY8xi-tGukWrdOCQvRnTB7lpOmST9HyeFPs',
-    client_secret: 'EFgw0p-C-rJIO9zFPAG1TiTLrOhachKpYecwpyEdTJjskhw3LFLG7b-tfnN2xstXCjQuwAhegMNExaJW',
+    mode:'sandbox',
+    client_id:'AfStiWLxaD4yEsZXiqLJcyUPTZZr8ykW0BoLPsBtWERO0OY8xi-tGukWrdOCQvRnTB7lpOmST9HyeFPs',
+    client_secret:'EFgw0p-C-rJIO9zFPAG1TiTLrOhachKpYecwpyEdTJjskhw3LFLG7b-tfnN2xstXCjQuwAhegMNExaJW'
 });
 
 module.exports = paypal;
