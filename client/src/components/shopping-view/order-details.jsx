b598dfa5a085d2c535537622b8851a83f31d0f64
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-08-06 23:21:19 +0530

    order details view update

--- client/src/components/shopping-view/order-details.jsx
+++ client/src/components/shopping-view/order-details.jsx
@@ -3,8 +3,12 @@ import { DialogContent } from '../ui/dialog'
 import { Label } from '../ui/label'
 import { Separator } from '../ui/separator'
 import { Badge } from '../ui/badge'
+import { useSelector } from 'react-redux'
 
 function ShoppingOrderDetailsView({orderDetails}) {
+ 
+ const {user} =  useSelector(state => state.auth)
+
   return (
      <DialogContent className="sm:max-w-[600px]">
         <div className="grid gap-6">
@@ -20,6 +24,14 @@ function ShoppingOrderDetailsView({orderDetails}) {
                 <div className="mt-2 flex items-center justify-between">
                     <p className="font-medium">Order Price</p>
                     <Label>${orderDetails?.totalAmount}</Label>
+                </div>
+                <div className="mt-2 flex items-center justify-between">
+                    <p className="font-medium">Payment method</p>
+                    <Label>{orderDetails?.paymentMethod}</Label>
+                </div>
+                <div className="mt-2 flex items-center justify-between">
+                    <p className="font-medium">Payment Status</p>
+                    <Label>{orderDetails?.paymentStatus}</Label>
                 </div>
                 <div className="mt-2 flex items-center justify-between">
                     <p className="font-medium">Order Status</p>
@@ -43,10 +55,17 @@ function ShoppingOrderDetailsView({orderDetails}) {
                     <div className="grid gap-2">
                         <div className="font-medium">Order Details</div>
                         <ul className="grid gap-3">
+                            {
+                                orderDetails?.cartItems && orderDetails?.cartItems.length > 0 ?
+                                orderDetails?.cartItems.map(item=>
                             <li className="flex items-center justify-between">
-                                <span>Product line 1</span>
-                                <span>$50</span> 
+                                <span>{item.title}</span>
+                                <span>X{item.quantity}</span>
+                                <span>${item.price}</span> 
                             </li>
+                                ) : null
+                            }
+                          
                         </ul>
                     </div>
               </div>
@@ -55,12 +74,12 @@ function ShoppingOrderDetailsView({orderDetails}) {
                     <div className="grid gap-2">
                         <div className="font-medium">Shipping Info</div>
                                 <div className="grid gap-0 5 text-muted-foreground">
-                                    <span>Nadun Dananjaya</span>
-                                    <span>123 Main St, Anytown, USA</span>
-                                    <span>matara</span>
-                                    <span>Maod8756</span>
-                                    <span>074556321</span>
-                                    <span>notes</span>
+                                    <span>{user.userName}</span>
+                                    <span>{orderDetails?.addressInfo?.address}</span>
+                                    <span>{orderDetails?.addressInfo?.city}</span>
+                                    <span>{orderDetails?.addressInfo?.pincode}</span>
+                                    <span>{orderDetails?.addressInfo?.phone}</span>
+                                    <span>{orderDetails?.addressInfo?.notes}</span>
                                 </div>
                     </div>
 
