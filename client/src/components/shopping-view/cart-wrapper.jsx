80731821cf2a9a7bb69865f9b30b341223352be6
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-08-09 02:16:47 +0530

    layout update cart wrapper

--- client/src/components/shopping-view/cart-wrapper.jsx
+++ client/src/components/shopping-view/cart-wrapper.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
 import { SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 import UserCartItemsContent from "./cart-items-content";
+import { Separator } from "../ui/separator";
 
 function UserCartWrapper({ cartItems, setOpenCartSheet }) {
   const navigate = useNavigate();
@@ -20,18 +21,33 @@ function UserCartWrapper({ cartItems, setOpenCartSheet }) {
       : 0;
 
   return (
-    <SheetContent className="sm:max-w-md">
-      <SheetHeader>
-        <SheetTitle>Your Cart</SheetTitle>
+    <SheetContent className="sm:max-w-md px-5">
+      <SheetHeader className="border-b pb-4">
+        <SheetTitle className="text-2xl font-bold text-gray-800">
+          Shopping Cart
+        </SheetTitle>
+        <p className="text-sm text-gray-500">
+          Review your items before proceeding to checkout.
+        </p>
       </SheetHeader>
-      <div className="mt-8 space-y-4">
+
+     <div className="mt-8 space-y-4 max-h-150 overflow-y-auto pr-2">
         {cartItems && cartItems?.items?.length > 0
-          ? cartItems.items.map((item) => <UserCartItemsContent cartItem={item} />)
+          ? cartItems.items.map((item, index) => (
+              <div
+                key={index}
+                className="transition-all duration-300 hover:shadow-md hover:scale-[1.02] rounded-lg"
+              >
+                <UserCartItemsContent cartItem={item} />
+              </div>
+            ))
           : null}
       </div>
+
+      <Separator/>
       <div className="mt-8 space-y-4">
         <div className="flex justify-between">
-          <span className="font-bold">Total</span>
+          <span className="font-bold">Total Amount :</span>
           <span className="font-bold">${totalCartAmount}</span>
         </div>
       </div>
